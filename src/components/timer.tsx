5ad67f20f04002694f26ef67e2f7c7880b022f6b
Manuel <delpinom@alot.com.ar>
2024-11-28 17:23:29 -0300

    fix: update asset paths in Timer component for correct background and logo images

--- src/components/timer.tsx
+++ src/components/timer.tsx
@@ -54,15 +54,15 @@ const Timer: React.FC<TimerProps> = ({ onTimeUp }) => {
   return (
     <div
       className="flex flex-col justify-center items-center w-screen h-screen bg-cover bg-center"
-      style={{ backgroundImage: "url('src/assets/bg.png')" }}
+      style={{ backgroundImage: "url('public/bg.png')" }}
     >
-      <img src="src\assets\logo.png" alt="Logo" className="w-64" />
+      <img src="public/logo.png" alt="Logo" className="w-64" />
       <div
         className="text-4xl sm:text-8xl font-bold text-transparent bg-clip-text"
         id="timer"
         style={{
           backgroundImage: "linear-gradient(45deg, #dfe44e, #ef4b85)",
-          textShadow: "2px 2px 4px #00000010",
+          textShadow: "2px 2px 4px 10px #00000025",
         }}
       >
         {timerComponents.length ? timerComponents : <span>Time's up!</span>}
