e467657c664cb99b52c9088f96a35e1f0f12e170
Manuel <delpinom@alot.com.ar>
2024-12-13 19:15:32 -0300

    fix: replace link with button to open PDF document in a new tab

--- src/App.tsx
+++ src/App.tsx
@@ -141,12 +141,10 @@ function App() {
         </a>
         {/* WHEN TIME IS OVER SHOW THIS ICON */}
         {!timerComponents.length && (
-          <a
-            href="#"
-            target="_blank"
-            rel="nonreferrer"
+            <button
+            onClick={() => window.open('/fiesta-da/public/Premios.pdf', '_blank')}
             className="w-14 h-14 p-2 rounded-full bg-[#dee679] hover:bg-[#e53f95] transition active:scale-95"
-          >
+            >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
@@ -156,7 +154,7 @@ function App() {
             >
               <path d="M8.243 7.34l-6.38 .925l-.113 .023a1 1 0 0 0 -.44 1.684l4.622 4.499l-1.09 6.355l-.013 .11a1 1 0 0 0 1.464 .944l5.706 -3l5.693 3l.1 .046a1 1 0 0 0 1.352 -1.1l-1.091 -6.355l4.624 -4.5l.078 -.085a1 1 0 0 0 -.633 -1.62l-6.38 -.926l-2.852 -5.78a1 1 0 0 0 -1.794 0l-2.853 5.78z"></path>
             </svg>
-          </a>
+            </button>
         )}
       </div>
     </div>
